0795d5d42fd75b6c4055c6570baa941d1b258108
zeyad hegazy <zhegazy@barmagli.com>
2025-05-06 08:55:14 +0300

    project init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        DB::table('roles')->insert([
+            ['name' => 'admin', 'label' => 'Administrator'],
+            ['name' => 'hr', 'label' => 'HR Manager'],
+            ['name' => 'employee', 'label' => 'Employee'],
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'role_id' => 1
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+});
+
+Route::middleware(['auth', 'role:hr,admin'])->group(function () {
+});
+
+Route::middleware(['auth', 'role:employee,admin'])->group(function () {
+});
+
+require __DIR__.'/auth.php';
